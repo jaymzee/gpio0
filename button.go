b56862c9102a67dca768841008fc3ab97b39d35a
jaymzee <jaymzee@users.noreply.github.com>
2020-11-07 06:44:57 -0500

    clearer code and added error handling

--- button.go
+++ button.go
@@ -2,7 +2,6 @@ package gpio0
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -12,27 +11,35 @@ type Button struct {
 	file *os.File
 }
 
-// NewButton creates a new Button for the gpio pin number given
-func NewButton(pin int) *Button {
+// OpenButton creates a new Button for the gpio pin number given
+func OpenButton(pin int) (*Button, error) {
 	filename := fmt.Sprintf("/sys/class/gpio/gpio%d/value", pin)
 
 	file, err := os.Open(filename)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
-	return &Button{pin: pin, file: file}
+	return &Button{pin, file}, nil
 }
 
-// Pressed return true if the button is pressed (active low)
-func (btn *Button) Pressed() bool {
-	buffer := make([]byte, 16)
+// Value returns the value read at the gpio pin
+func (btn *Button) Value() (int, error) {
+	buf := make([]byte, 1)
+	_, err := btn.file.ReadAt(buf, 0)
+	if err != nil {
+		return 0, err
+	}
+	return int(buf[0] - '0'), nil
+}
 
-	btn.file.Seek(0, io.SeekStart)
-	count, err := btn.file.Read(buffer)
+// Pressed returns true if the button is pressed (active low)
+// To keep usage of this function simple (e.g. interfaces),
+// it will panic if an error is incurred while writing to the gpio pin
+func (btn *Button) Pressed() bool {
+	val, err := btn.Value()
 	if err != nil {
 		panic(err)
 	}
-
-	return !(count > 0 && buffer[0] == '1')
+	return val == 0
 }
--- led.go
+++ led.go
@@ -2,7 +2,6 @@ package gpio0
 
 import (
 	"fmt"
-	"io"
 	"os"
 )
 
@@ -12,33 +11,47 @@ type LED struct {
 	file *os.File
 }
 
-// NewLED returns a new LED for the gpio pin given
-func NewLED(pin int) *LED {
+// OpenLED returns an LED for the gpio pin at /sys/class/gpio/gpio{n}
+func OpenLED(pin int) (*LED, error) {
 	filename := fmt.Sprintf("/sys/class/gpio/gpio%d/value", pin)
 	file, err := os.OpenFile(filename, os.O_WRONLY, 0)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	return &LED{pin: pin, file: file}
+	return &LED{pin: pin, file: file}, nil
 }
 
-// Set sets the LED to v
-func (led *LED) Set(v bool) {
-	if v {
-		led.On()
-	} else {
-		led.Off()
-	}
+// Close closes the file in /sys/class/gpio
+func (led *LED) Close() error {
+	return led.file.Close()
 }
+
+// Set sets the LED to v (active low)
+func (led *LED) Set(v bool) error {
+	var b byte = '1'
+	if v {
+		b = '0'
+	}
+	_, err := led.file.WriteAt([]byte{b}, 0)
+	return err
+}
+
+// The functions below satisfy a very simple On/Off interface.
+// To keep the error handling very simple for users of this interface,
+// these functions panic on error.
 
 // On turns on led (active low)
 func (led *LED) On() {
-	led.file.Seek(0, io.SeekStart)
-	led.file.Write([]byte{'0'})
+	err := led.Set(true)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // Off turns off led (active low)
 func (led *LED) Off() {
-	led.file.Seek(0, io.SeekStart)
-	led.file.Write([]byte{'1'})
+	err := led.Set(false)
+	if err != nil {
+		panic(err)
+	}
 }
